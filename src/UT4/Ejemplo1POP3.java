a43826607c78f828556553443d25c4dca46c10e6
scuestam <sebastian.cuesta.molto@gmail.com>
2020-02-11 09:32:48 +0100

    Actu 11 Feb 09:32

--- src/UT4/Ejemplo1POP3.java
+++ src/UT4/Ejemplo1POP3.java
@@ -3,6 +3,7 @@ package UT4;
 import org.apache.commons.net.pop3.POP3MessageInfo;
 import org.apache.commons.net.pop3.POP3SClient;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.net.SocketException;
 
@@ -39,4 +40,33 @@ public class Ejemplo1POP3 {
         }
         System.exit(0);
     }// main
+
+    private static void RecuperaMensajes(POP3MessageInfo[] men, POP3SClient pop3) throws IOException{
+        for(int i = 0;i<men.length;i++){
+            System.out.println("Mensaje: "+(i+1));
+            POP3MessageInfo msginfo = men [i]; // lista de mensajes
+            System.out.println("IDentificador: "+msginfo.identifier+", Number:"+msginfo.number+", Tamaño: "+msginfo.size);
+
+            System.out.println("Prueba de listUniqueIdentifier: ");
+            POP3MessageInfo pmi =pop3.listUniqueIdentifier(i+1); // un mensaje
+            System.out.println("\tIDentificador: "+pmi.identifier+", Number: "+pmi.number+", Tamaño: "+pmi.size);
+        } // for
+    } // Recuperamensajes
+
+    private static void RecuperaCabeceras(POP3MessageInfo[] men, POP3SClient pop3) throws IOException{
+        for(int i = 0;i<men.length;i++){
+            System.out.println("Mensaje: "+(i+1));
+            POP3MessageInfo msginfo = men [i]; // lista de mensajes
+
+            // solo recupera la cabecera
+            System.out.println("Cabecera del mensaje: ");
+            BufferedReader reader = (BufferedReader) pop3.retrieveMessageTop(msginfo.number,0);
+            String linea;
+            while((linea=reader.readLine()) != null){
+                System.out.println(linea.toString());
+            }
+            reader.close();
+        } // for
+    } // Recuperacabeceras
+
 } // ..Ejemplo1POP3
